2bb26efdf2c8a7760d0428685260e5aceab6f2e1
MBoota <m43577535@gmail.com>
2024-02-03 19:32:03 +0500

    First Middlware

--- Middlware/Program.cs
+++ Middlware/Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//======================================
+//------------  Middlware --------------
+//======================================
+
+//______ 1. Run Middlware ______
+#region Run
+app.Run(async (HttpContext context) =>
+{
+    await context.Response.WriteAsync("My First Middlware");
+});
+#endregion
 
 app.Run();
