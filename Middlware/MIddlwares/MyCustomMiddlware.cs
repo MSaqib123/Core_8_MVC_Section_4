bf97c4a926e2e08a96c1ec4a5486d839092dd531
MBoota <m43577535@gmail.com>
2024-02-03 21:03:45 +0500

    Mutliple Middlwares

---
+++ Middlware/MIddlwares/MyCustomMiddlware.cs
@@ -0,0 +1,13 @@
+﻿namespace Middlware.MIddlwares
+{
+    public class MyCustomMiddlware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            context.Response.ContentType = "text/html";
+            await context.Response.WriteAsync("Start Middleware");
+            await next(context);
+            await context.Response.WriteAsync("end Middleware");
+        }
+    }
+}
--- Middlware/Program.cs
+++ Middlware/Program.cs
@@ -1,4 +1,10 @@
+using Middlware.MIddlwares;
+
 var builder = WebApplication.CreateBuilder(args);
+//======================================
+//----------  Register Middlware ------------
+//======================================
+builder.Services.AddTransient<MyCustomMiddlware>();
 var app = builder.Build();
 
 //======================================
@@ -7,10 +13,45 @@ var app = builder.Build();
 
 //______ 1. Run Middlware ______
 #region Run
+//app.Run(async (HttpContext context) =>
+//{
+//    await context.Response.WriteAsync("My First Middlware");
+//});
+#endregion
+
+//______ 2. Multiple Middlware (RequestDelegate) ______
+#region by app.Use
+//app.Use(async (HttpContext context, RequestDelegate next) =>
+//{
+//    await context.Response.WriteAsync("My First Middlware");
+//    await next(context);
+//});
+
+//app.Use(async (HttpContext context, RequestDelegate next) =>
+//{
+//    await context.Response.WriteAsync("My 2nd Middlware");
+//    await next(context);
+//});
+
+//app.Run(async (HttpContext context) =>
+//{
+//    await context.Response.WriteAsync("My 3rd Middlware");
+//});
+#endregion
+
+
+//______ 3. (Code Managing) Custom Class Middlware  ______
+#region 
+//Custom Middwlare
+app.UseMiddleware<MyCustomMiddlware>();
+
+//2nd middwlare
 app.Run(async (HttpContext context) =>
 {
-    await context.Response.WriteAsync("My First Middlware");
+    await context.Response.WriteAsync("<br> My 3rd Middlware <br>");
 });
 #endregion
+
+
 
 app.Run();
